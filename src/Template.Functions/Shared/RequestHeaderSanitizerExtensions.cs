19a17bac636f180767571c5a368c7f465072d724
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2024-02-01 12:17:27 +0000

    fix: sanitize security headers should not be case sensitive

--- src/Template.Functions/Shared/RequestHeaderSanitizerExtensions.cs
+++ src/Template.Functions/Shared/RequestHeaderSanitizerExtensions.cs
@@ -6,17 +6,20 @@ using Microsoft.Net.Http.Headers;
 namespace Template.Functions.Shared;
 
 public static class RequestHeaderSanitizerExtensions {
-  private static readonly Regex JwtClaimPattern = new(@"Bearer\s+(?<header>[^.]+)\.(?<payload>[^.]+)\.(?<signature>[^.]+)");
+  private static readonly Regex JwtClaimPattern =
+    new(@"Bearer\s+(?<header>[^.]+)\.(?<payload>[^.]+)\.(?<signature>[^.]+)");
 
   public static IHeaderDictionary SanitizeJwtTokenAuthzHeader(this IHeaderDictionary headers,
     params string[] additional) {
-    if (!(headers.ContainsKey(HeaderNames.Authorization) || additional.Any(headers.ContainsKey))) {
+    if (!(headers.Keys.Contains(HeaderNames.Authorization, StringComparer.OrdinalIgnoreCase) ||
+          additional.Any(h => headers.Keys.Contains(h, StringComparer.OrdinalIgnoreCase)))) {
       return headers;
     }
 
     var sanitizedHeaders = new HeaderDictionary();
     foreach (var header in headers) {
-      if (header.Key == HeaderNames.Authorization || additional.Contains(header.Key)) {
+      if (StringComparer.OrdinalIgnoreCase.Equals(header.Key, HeaderNames.Authorization) ||
+          additional.Contains(header.Key, StringComparer.OrdinalIgnoreCase)) {
         var sanitizedValues = new StringValues(header.Value.Select(SanitizeJwtToken).ToArray());
         sanitizedHeaders.Add(header.Key, sanitizedValues);
       } else {
