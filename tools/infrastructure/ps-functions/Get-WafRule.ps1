301c85e312dcf4219838c39e58c4a6444ba154d7
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2025-04-14 19:10:27 +0100

    chore: demo env WAF rule script fix

--- tools/infrastructure/ps-functions/Get-WafRule.ps1
+++ tools/infrastructure/ps-functions/Get-WafRule.ps1
@@ -18,11 +18,11 @@ function Get-WafRule {
         $callerEA = $ErrorActionPreference
         $ErrorActionPreference = 'Stop'
         
-        . "$PSScriptRoot/Get-IsEnvironmentProdLike.ps1"
+        . "$PSScriptRoot/Get-IsPublicHostNameProdLike.ps1"
         . "$PSScriptRoot/Get-PublicHostName.ps1"
         . "$PSScriptRoot/Get-CloudflareWafRuleExpr.ps1"
 
-        $isEnvProdLike = Get-IsEnvironmentProdLike $EnvironmentName
+        $isEnvProdLike = Get-IsPublicHostNameProdLike $EnvironmentName
         $isHostHeaderFilterRequired = $Domain['SubDomainLevel'] -eq 2 -or(!$isEnvProdLike -and($Domain['NonProdSubDomain'] -ne 'UseProductDomain'))
         $hostHeaderWafFilter = $isHostHeaderFilterRequired ? $HostName : $null
         $zoneName = ((Get-PublicHostName $EnvironmentName @Domain).Split('.') | Select-Object -Skip 1) -join '.'
