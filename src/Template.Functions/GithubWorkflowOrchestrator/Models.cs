c077c568decffcee2c53189f19ce34c29f4be765
Sumedh Bhat <sumedh.bhat@mrisoftware.com>
2026-01-30 18:20:06 +0530

    feat(app): Facilitated flag to rerun jobs or entire workflows

--- src/Template.Functions/GithubWorkflowOrchestrator/Models.cs
+++ src/Template.Functions/GithubWorkflowOrchestrator/Models.cs
@@ -7,26 +7,22 @@ public class OrchestratorInput {
   [Range(1, int.MaxValue)]
   public int MaxAttempts { get; set; }
 
-  [Required]
-  public TimeSpan Timeout { get; set; }
+  [Required] public TimeSpan Timeout { get; set; }
 
-  [Required]
-  public bool RerunEntireWorkflow { get; set; }
+  [Required] public bool RerunEntireWorkflow { get; set; }
 
-  [Required]
-  public string WorkflowFile { get; set; } = null!;
+  [Required] public string WorkflowFile { get; set; } = null!;
 }
 
 public class StartWorkflowRequest {
-  [Required]
-  public string WorkflowFile { get; set; } = null!;
+  [Required] public string WorkflowFile { get; set; } = null!;
+
+  public bool RerunEntireWorkflow { get; set; } = false;
 }
 
 public class TriggerInput {
-  [Required]
-  public string InstanceId { get; set; } = null!;
+  [Required] public string InstanceId { get; set; } = null!;
 
-  [Required]
-  public string WorkflowFile { get; set; } = null!;
+  [Required] public string WorkflowFile { get; set; } = null!;
 }
 
